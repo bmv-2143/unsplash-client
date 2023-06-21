ede86edaec9aeed6ef77d1198f9948bba9263ac2
bmv-2143 <bmv-2143@yandex.ru>
2023-06-21 22:25:58 +0300

    feat(uc1): add onboarding fragments, WIP

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/onboarding/OnboardingActivity.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/onboarding/OnboardingActivity.kt
@@ -7,6 +7,14 @@ import com.example.unsplashattestationproject.R
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboarding)
+
+        addFragment()
+    }
+
+    private fun addFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.activity_onboarding_fragment_container, OnboardingCreateFragment())
+            .commit()
     }
 }
