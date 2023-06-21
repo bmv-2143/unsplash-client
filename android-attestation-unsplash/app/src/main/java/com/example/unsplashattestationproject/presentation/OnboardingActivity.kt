86b08273b777a1497c3c95a284ac25b74779814b
bmv-2143 <bmv-2143@yandex.ru>
2023-06-21 22:15:20 +0300

    refactor(uc1): rename main activity to onboarding activity

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/OnboardingActivity.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/OnboardingActivity.kt
@@ -1,9 +1,10 @@
-package com.example.unsplashattestationproject
+package com.example.unsplashattestationproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.unsplashattestationproject.R
 
-class MainActivity : AppCompatActivity() {
+class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
