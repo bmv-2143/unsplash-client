2fbb832e677d0f712ff2ccc4f00691e5215175c5
bmv-2143 <bmv-2143@yandex.ru>
2023-06-21 22:16:11 +0300

    refactor(uc1): rename / move main activity to onboarding activity

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/onboarding/OnboardingActivity.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/onboarding/OnboardingActivity.kt
@@ -1,4 +1,4 @@
-package com.example.unsplashattestationproject.presentation
+package com.example.unsplashattestationproject.presentation.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
