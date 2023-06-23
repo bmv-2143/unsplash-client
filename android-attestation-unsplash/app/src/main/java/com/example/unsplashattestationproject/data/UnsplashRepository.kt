58981aed0fc8045717f78ba183c97e8729d02b4d
bmv-2143 <bmv-2143@yandex.ru>
2023-06-23 17:36:53 +0300

    feat(uc2): save access_token to shared preferences

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/UnsplashRepository.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/UnsplashRepository.kt
@@ -1,9 +1,14 @@
 package com.example.unsplashattestationproject.data
 
+import android.content.SharedPreferences
 import android.util.Log
+import com.example.unsplashattestationproject.log.TAG
 import javax.inject.Inject
 
-class UnsplashRepository @Inject constructor(private val unsplashNetworkDataSource: UnsplashNetworkDataSource) {
+class UnsplashRepository @Inject constructor(
+    private val unsplashNetworkDataSource: UnsplashNetworkDataSource,
+    private val sharedPreferences: SharedPreferences
+    ) {
 
     suspend fun getAccessToken(authCode: String) : String {
         kotlin.runCatching {
@@ -11,14 +16,21 @@ class UnsplashRepository @Inject constructor(private val unsplashNetworkDataSour
             authInfo
         }.fold(
             onSuccess = { authInfo ->
-                Log.e("TOKEN", authInfo.accessToken)
+                Log.e(TAG, "Authorization success, access token: ${authInfo.accessToken}")
+                saveAccessToken(authInfo.accessToken)
                 return authInfo.accessToken
             },
-            onFailure = {
-                Log.e("TOKEN", it.message ?: "")
+            onFailure = { throwable ->
+                Log.e(TAG, "Authorization failed, error: ${throwable.message}")
                 return ""
             }
         )
+    }
+
+    private fun saveAccessToken(accessToken: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("access_token", accessToken)
+        editor.apply()
     }
 
 }
