3bdaaf510f1c6426f32a08d021691334246351f0
bmv-2143 <bmv-2143@yandex.ru>
2023-06-24 12:40:49 +0300

    fix(uc3): add DI annotation to UserProfileViewModel

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/user_profile/UserProfileViewModel.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/user_profile/UserProfileViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class UserProfileViewModel : ViewModel() {
+class UserProfileViewModel @Inject constructor(): ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "USER PROFILE"
