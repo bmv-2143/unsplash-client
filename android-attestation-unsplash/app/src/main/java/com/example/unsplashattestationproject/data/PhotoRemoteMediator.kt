66b37649f7266f9e39a948017a5c644b8a10c226
bmv-2143 <bmv-2143@yandex.ru>
2023-06-27 22:30:24 +0300

    refactor(uc3): reimplement RemoteMediator with RemoteKeys objects

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/PhotoRemoteMediator.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/PhotoRemoteMediator.kt
@@ -1,5 +1,7 @@
 package com.example.unsplashattestationproject.data
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
@@ -8,7 +10,13 @@ import androidx.room.withTransaction
 import com.example.unsplashattestationproject.data.dto.photos.UnsplashPhoto
 import com.example.unsplashattestationproject.data.room.PhotoDatabase
 import com.example.unsplashattestationproject.data.room.entities.Photo
+import com.example.unsplashattestationproject.data.room.entities.RemoteKeys
+import retrofit2.HttpException
+import java.io.IOException
 import javax.inject.Inject
+
+// GitHub page API is 1 based: https://developer.github.com/v3/#pagination
+private const val UNSPLASH_STARTING_PAGE_INDEX = 1
 
 @OptIn(ExperimentalPagingApi::class)
 class PhotoRemoteMediator @Inject constructor(
@@ -16,51 +24,107 @@ class PhotoRemoteMediator @Inject constructor(
     private val photoDatabase: PhotoDatabase
 
 ) : RemoteMediator<Int, Photo>() {
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Photo>): MediatorResult {
 
-    private var nextPage: Int? = null
+        Log.e(TAG, "load: loadType = $loadType, state = $state")
 
-    override suspend fun load(
-        loadType: LoadType,
-        state: PagingState<Int, Photo>
-    ): MediatorResult {
-        // Get the page key for the current load
-        val pageKey = when (loadType) {
+        val page = when (loadType) {
             LoadType.REFRESH -> {
-                nextPage = null
-                null
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1) ?: UNSPLASH_STARTING_PAGE_INDEX
             }
-            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
-            LoadType.APPEND -> nextPage
+            LoadType.PREPEND -> {
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                // If remoteKeys is null, that means the refresh result is not in the database yet.
+                // We can return Success with `endOfPaginationReached = false` because Paging
+                // will call this method again if RemoteKeys becomes non-null.
+                // If remoteKeys is NOT NULL but its prevKey is null, that means we've reached
+                // the end of pagination for prepend.
+                val prevKey = remoteKeys?.prevKey
+                if (prevKey == null) {
+                    return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                }
+                prevKey
+            }
+            LoadType.APPEND -> {
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                // If remoteKeys is null, that means the refresh result is not in the database yet.
+                // We can return Success with `endOfPaginationReached = false` because Paging
+                // will call this method again if RemoteKeys becomes non-null.
+                // If remoteKeys is NOT NULL but its nextKey is null, that means we've reached
+                // the end of pagination for append.
+                val nextKey = remoteKeys?.nextKey
+                if (nextKey == null) {
+                    return MediatorResult.Success(endOfPaginationReached = remoteKeys != null)
+                }
+                nextKey
+            }
         }
 
-        // Fetch data from the Unsplash API
-        val response = try {
-            unsplashNetworkDataSource.getPhotos(
-                page = pageKey ?: 1,
-                perPage = state.config.pageSize
-            )
-        } catch (e: Exception) {
-            return MediatorResult.Error(e)
+//        val apiQuery = query + IN_QUALIFIER
+
+        try {
+            val photos = unsplashNetworkDataSource.getPhotos(page, state.config.pageSize)
+
+//            val repos = apiResponse.items
+
+            val endOfPaginationReached = photos.isEmpty()
+
+            photoDatabase.withTransaction {
+
+                // clear all tables in the database
+                if (loadType == LoadType.REFRESH) {
+                    photoDatabase.remoteKeysDao().clearRemoteKeys()
+                    photoDatabase.photoDao().clearPhotos()
+                }
+                val prevKey = if (page == UNSPLASH_STARTING_PAGE_INDEX) null else page - 1
+                val nextKey = if (endOfPaginationReached) null else page + 1
+
+                val keys = photos.map {
+                    RemoteKeys(photoId = it.id, prevKey = prevKey, nextKey = nextKey)
+                }
+                photoDatabase.remoteKeysDao().insertAll(keys)
+                photoDatabase.photoDao().insertPhotos(photos.map { it.toPhoto() })
+            }
+
+            return MediatorResult.Success(endOfPaginationReached = endOfPaginationReached)
+
+        } catch (exception: IOException) {
+            return MediatorResult.Error(exception)
+        } catch (exception: HttpException) {
+            return MediatorResult.Error(exception)
         }
-
-        // Store the data in the Room database
-        cacheResponseInDb(loadType, pageKey, response)
-
-        // Return success with endOfPaginationReached set to true if no more data is available
-        return MediatorResult.Success(endOfPaginationReached = response.isEmpty())
     }
 
-    private suspend fun cacheResponseInDb(
-        loadType: LoadType,
-        pageKey: Int?,
-        response: List<UnsplashPhoto>
-    ) {
-        photoDatabase.withTransaction {
-            if (loadType == LoadType.REFRESH) {
-                clearDatabase()
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Photo>): RemoteKeys? {
+        // Get the last page that was retrieved, that contained items.
+        // From that last page, get the last item
+        return state.pages.lastOrNull() { it.data.isNotEmpty() }?.data?.lastOrNull()
+            ?.let { photo ->
+                // Get the remote keys of the last item retrieved
+                photoDatabase.remoteKeysDao().remoteKeysPhotoId(photo.id)
             }
-            nextPage = pageKey?.plus(1)
-            savePhotosToDb(response)
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Photo>): RemoteKeys? {
+        // Get the first page that was retrieved, that contained items.
+        // From that first page, get the first item
+        return state.pages.firstOrNull { it.data.isNotEmpty() }?.data?.firstOrNull()
+            ?.let { photo ->
+                // Get the remote keys of the first items retrieved
+                photoDatabase.remoteKeysDao().remoteKeysPhotoId(photo.id)
+            }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(
+        state: PagingState<Int, Photo>
+    ): RemoteKeys? {
+        // The paging library is trying to load data after the anchor position
+        // Get the item closest to the anchor position
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { photoId ->
+                photoDatabase.remoteKeysDao().remoteKeysPhotoId(photoId)
+            }
         }
     }
 
--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/room/PhotoDatabase.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/room/PhotoDatabase.kt
@@ -3,11 +3,15 @@ package com.example.unsplashattestationproject.data.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.unsplashattestationproject.data.room.daos.PhotoDao
+import com.example.unsplashattestationproject.data.room.daos.RemoteKeysDao
 import com.example.unsplashattestationproject.data.room.entities.Photo
+import com.example.unsplashattestationproject.data.room.entities.RemoteKeys
 
-@Database(entities = [Photo::class], version = 1)
+@Database(entities = [Photo::class, RemoteKeys::class], version = 1)
 abstract class PhotoDatabase : RoomDatabase() {
 
     abstract fun photoDao(): PhotoDao
+
+    abstract fun remoteKeysDao(): RemoteKeysDao
 
 }
