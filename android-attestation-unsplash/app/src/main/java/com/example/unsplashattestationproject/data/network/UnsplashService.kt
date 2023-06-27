763f18e0b71ef7da080f0bdfc0c1a31bf4e6d09e
bmv-2143 <bmv-2143@yandex.ru>
2023-06-28 00:08:10 +0300

    fix(uc3): make unsplash service singleton

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/network/UnsplashService.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/network/UnsplashService.kt
@@ -3,7 +3,9 @@ package com.example.unsplashattestationproject.data.network
 import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Named
+import javax.inject.Singleton
 
+@Singleton
 class UnsplashService @Inject constructor(@Named("api") retrofitUnsplashService: Retrofit) {
 
     val unsplashApi: UnsplashApi =
