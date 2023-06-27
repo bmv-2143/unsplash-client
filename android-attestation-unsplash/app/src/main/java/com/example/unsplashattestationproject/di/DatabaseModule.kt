be7aa4cb82beb47b05bafb2054edb9a0c090bc3a
bmv-2143 <bmv-2143@yandex.ru>
2023-06-27 23:16:10 +0300

    fix(uc3): fix annotations in DatabaseModule (add @Singleton)

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/di/DatabaseModule.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/di/DatabaseModule.kt
@@ -9,14 +9,17 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
+    @Singleton
     @Provides
     fun providePhotoDao(database: PhotoDatabase) = database.photoDao()
 
+    @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): PhotoDatabase {
         return Room.databaseBuilder(
