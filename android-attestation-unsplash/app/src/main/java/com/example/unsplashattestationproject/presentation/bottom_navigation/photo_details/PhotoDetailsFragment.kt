e907909816932509bca4f66e155823924878c173
bmv-2143 <bmv-2143@yandex.ru>
2023-06-29 00:47:43 +0300

    feat(uc4): implement photo details fragment location rendering

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/photo_details/PhotoDetailsFragment.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/photo_details/PhotoDetailsFragment.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.example.unsplashattestationproject.data.dto.photos.UnsplashPhotoDetails
 import com.example.unsplashattestationproject.databinding.FragmentPhotoDetailsBinding
 import com.example.unsplashattestationproject.presentation.bottom_navigation.BottomNavigationActivityViewModel
 import com.example.unsplashattestationproject.presentation.bottom_navigation.photo_list.PhotoItemLoader
@@ -26,7 +28,7 @@ class PhotoDetailsFragment : Fragment() {
     private val binding get() = _binding!!
     private val activityViewModel: BottomNavigationActivityViewModel by activityViewModels()
 
-    private val photoDetailsFragmentViewModel : PhotoDetailsFragmentViewModel by viewModels()
+    private val photoDetailsFragmentViewModel: PhotoDetailsFragmentViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,8 +49,15 @@ class PhotoDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setClickListeners()
         observerPhotoDetails()
         photoDetailsFragmentViewModel.loadPhotoDetails(activityViewModel.selectedPhoto!!.remoteId)
+    }
+
+    private fun setClickListeners() {
+        binding.fragmentPhotoDetailsLocation.setOnClickListener {
+            Toast.makeText(requireContext(), "Location", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun observerPhotoDetails() {
@@ -56,9 +65,30 @@ class PhotoDetailsFragment : Fragment() {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 photoDetailsFragmentViewModel.photoDetailsFlow.collect { photoDetails ->
                     Log.e(TAG, "PHOTO DETAILS: $photoDetails")
+                    updateLocation(photoDetails)
                 }
             }
         }
+    }
+
+    private fun updateLocation(photoDetails: UnsplashPhotoDetails) {
+        updateLocationVisibility(photoDetails)
+        setCityCountryText(photoDetails)
+    }
+
+    private fun setCityCountryText(photoDetails: UnsplashPhotoDetails) {
+        val cityCountry = listOfNotNull(photoDetails.location?.city, photoDetails.location?.country)
+        binding.fragmentPhotoDetailsLocationText.text =
+            cityCountry.joinToString(separator = ", ")
+    }
+
+    private fun updateLocationVisibility(photoDetails: UnsplashPhotoDetails) {
+        binding.fragmentPhotoDetailsLocation.visibility =
+            if (photoDetails.location?.city == null && photoDetails.location?.country == null) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
     }
 
     override fun onDestroyView() {
