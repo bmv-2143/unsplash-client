141d631d9e63512c752027522457211ed751a4ae
bmv-2143 <bmv-2143@yandex.ru>
2023-06-30 16:24:52 +0300

    fix(uc7): increase socket timeout to 30 seconds for unsplash service

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/di/NetworkModule.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/di/NetworkModule.kt
@@ -17,6 +17,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -55,6 +56,8 @@ class NetworkModule {
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .addInterceptor(authInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
             .build()
 
         return Retrofit.Builder().baseUrl(BuildConfig.API_URL)
