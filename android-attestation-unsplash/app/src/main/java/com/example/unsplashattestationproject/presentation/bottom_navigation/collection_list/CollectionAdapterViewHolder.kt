8063a3f5ce430483146ddb5e39f2f747ff694fdc
bmv-2143 <bmv-2143@yandex.ru>
2023-07-05 17:43:16 +0300

    feat(uc10): display each collection item in the UI

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/collection_list/CollectionAdapterViewHolder.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/collection_list/CollectionAdapterViewHolder.kt
@@ -1,11 +1,20 @@
 package com.example.unsplashattestationproject.presentation.bottom_navigation.collection_list
 
+import android.graphics.drawable.Drawable
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.unsplashattestationproject.R
 import com.example.unsplashattestationproject.data.dto.collections.UnsplashCollection
-import com.example.unsplashattestationproject.databinding.PhotoCollectionListItemBinding
+import com.example.unsplashattestationproject.databinding.CollectionListItemBinding
+import com.example.unsplashattestationproject.log.TAG
 
 class CollectionAdapterViewHolder(
-    private val binding: PhotoCollectionListItemBinding,
+    private val binding: CollectionListItemBinding,
     val onClick: (UnsplashCollection) -> Unit
 ) : RecyclerView.ViewHolder(binding.root) {
 
@@ -21,7 +30,72 @@ class CollectionAdapterViewHolder(
 
     fun bind(collectionItem: UnsplashCollection) {
         currentItem = collectionItem
-        binding.photoCollectionListItemText.text = collectionItem.id // TODO: add other fields
+        loadTexts(collectionItem)
+        loadAvatar(collectionItem)
+        loadPhoto(collectionItem.cover_photo?.urls?.regular ?: "")
     }
 
+    private fun loadTexts(collectionItem: UnsplashCollection) {
+        binding.collectionListItemNumberOfPhotos.text =
+            binding.root.context.getString(
+                R.string.collection_item_number_of_photos_template,
+                collectionItem.totalPhotos.toString()
+            )
+
+        binding.collectionListItemTitle.text = collectionItem.title
+        binding.collectionAuthorName.text = collectionItem.user.name
+
+        binding.collectionAuthorNickname.text =
+            binding.root.context.getString(
+                R.string.photo_item_author_nickname_template,
+                collectionItem.user.username
+            )
+    }
+
+    private fun loadAvatar(collectionItem: UnsplashCollection) {
+        Glide.with(binding.root)
+            .load(collectionItem.user.profileImageUrls.small)
+            .circleCrop()
+            .placeholder(R.drawable.photo_list_item_avatar_placeholder)
+            .into(binding.collectionAuthorAvatar)
+    }
+
+    private fun loadPhoto(imageUrl: String) {
+        progressBarSetVisible(binding, true)
+
+        Glide.with(binding.root.context)
+            .load(imageUrl)
+            .placeholder(R.drawable.photo_list_item_image_placeholder)
+            .listener(object : RequestListener<Drawable> {
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    progressBarSetVisible(binding, false)
+                    return false
+                }
+
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e(TAG, "Glide onLoadFailed: $imageUrl")
+                    return false
+                }
+            })
+            .into(binding.collectionItemImage)
+    }
+
+    private fun progressBarSetVisible(binding: CollectionListItemBinding, isActive: Boolean) =
+        if (isActive) {
+            binding.collectionItemProgressBar.visibility = android.view.View.VISIBLE
+        } else {
+            binding.collectionItemProgressBar.visibility = android.view.View.GONE
+        }
 }
--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/collection_list/CollectionPagedAdapter.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/presentation/bottom_navigation/collection_list/CollectionPagedAdapter.kt
@@ -4,14 +4,14 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import com.example.unsplashattestationproject.data.dto.collections.UnsplashCollection
-import com.example.unsplashattestationproject.databinding.PhotoCollectionListItemBinding
+import com.example.unsplashattestationproject.databinding.CollectionListItemBinding
 
 class CollectionPagedAdapter(
     private val onItemClicked: (UnsplashCollection) -> Unit
 ) : PagingDataAdapter<UnsplashCollection, CollectionAdapterViewHolder>(CollectionDiffUtilCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionAdapterViewHolder {
-        val binding = PhotoCollectionListItemBinding
+        val binding = CollectionListItemBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
         return CollectionAdapterViewHolder(binding, onItemClicked)
     }
