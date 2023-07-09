60acbbba966a7b4ddbecf79e0aa508ad91df84c8
bmv-2143 <bmv-2143@yandex.ru>
2023-07-10 01:42:32 +0300

    refactor: add Moshi annotations to TokenBody

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/dto/auth/TokenBody.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/data/dto/auth/TokenBody.kt
@@ -1,13 +1,15 @@
 package com.example.unsplashattestationproject.data.dto.auth
 
 import com.example.unsplashattestationproject.data.network.AuthQuery
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
 
+@JsonClass(generateAdapter = true)
 data class TokenBody(
-
-    val client_id: String = AuthQuery.VAL_ACCESS_KEY,
-    val client_secret: String = AuthQuery.CLIENT_ID_SECRET_KEY,
-    val redirect_uri: String = AuthQuery.VAL_REDIRECT_URL,
+    @Json(name = "client_id") val clientId: String = AuthQuery.VAL_ACCESS_KEY,
+    @Json(name = "client_secret") val clientSecret: String = AuthQuery.CLIENT_ID_SECRET_KEY,
+    @Json(name = "redirect_uri") val redirectUri: String = AuthQuery.VAL_REDIRECT_URL,
     val code: String,
-    val grant_type: String = AuthQuery.VAL_AUTH_CODE,
+    @Json(name = "grant_type") val grantType: String = AuthQuery.VAL_AUTH_CODE,
 )
