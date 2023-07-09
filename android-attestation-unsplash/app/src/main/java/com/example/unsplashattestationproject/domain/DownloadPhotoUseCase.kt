e8ddfe6560b8547a1651d5b0eb84fe308f0f049c
bmv-2143 <bmv-2143@yandex.ru>
2023-07-10 01:23:22 +0300

    !refactor: remove DownloadPhotoUseCase.getTrackedDownloadUrl()

--- android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/domain/DownloadPhotoUseCase.kt
+++ android-attestation-unsplash/app/src/main/java/com/example/unsplashattestationproject/domain/DownloadPhotoUseCase.kt
@@ -9,11 +9,4 @@ class DownloadPhotoUseCase @Inject constructor(private val unsplashRepository: U
     operator fun invoke(photo: UnsplashPhotoDetails) =
         unsplashRepository.startTrackedDownload(photo)
 
-    // todo: use case should contain only one method
-    suspend fun getTrackedDownloadUrl(photoId: String) : String? =
-        unsplashRepository.getTrackedDownloadPhotoUrl(photoId)
-
-    fun startTrackedDownload(url : String, photoId: String) =
-        unsplashRepository.startTrackedDownload(url, photoId)
-
 }
